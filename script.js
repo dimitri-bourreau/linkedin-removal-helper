5f16c392d2d0c6b44881a4139b66f5246d654f28
Dimitri Bourreau <contact@dimitribourreau.me>
2022-05-20 22:17:23 +0200

    refactor & improve performance

--- script.js
+++ script.js
@@ -1,84 +1,144 @@
-(async () => {
-	const listClassName = 'scaffold-finite-scroll__content';
-	const removeDropdownClassName = 'artdeco-dropdown__trigger--placement-bottom';
-	const peopleNameClassName = 'mn-connection-card__name';
-	const removeButtonClassName = 'mn-connection-card__dropdown-option-text';
-	const modalClassName = 'artdeco-modal';
-	const confirmRemoveButtonClassName = 'artdeco-modal__confirm-dialog-btn';
-	const removeText = 'Supprimer la relation';
+const listClassName = 'scaffold-finite-scroll__content';
+const removeDropdownClassName = 'artdeco-dropdown__trigger--placement-bottom';
+const peopleNameClassName = 'mn-connection-card__name';
+const modalClassName = 'artdeco-modal';
+const confirmRemoveButtonClassName = 'artdeco-modal__confirm-dialog-btn';
+const removeText = 'Supprimer la relation';
+const awaitTime = 200;
 
-	const list = document.getElementsByClassName(listClassName)[0];
-	const network = [...list.getElementsByTagName('li')];
-
+(async function linkedInRemovalHelper() {
 	let stop = false;
-
 	while (!stop) {
-		const list = document.getElementsByClassName(listClassName)[0];
-		const network = [...list.getElementsByTagName('li')];
-		await iterateOverNetwork(network);
-	}
-
-	async function iterateOverNetwork(network) {
-		return new Promise(async (resolve) => {
-			for (const people of network) {
-				const removeDropdown = people.getElementsByClassName(
-					removeDropdownClassName,
-				)[0];
-				const name =
-					people.getElementsByClassName(peopleNameClassName)[0].innerText;
-
-				people.scrollIntoView({
-					behavior: 'smooth',
-					block: 'end',
-					inline: 'nearest',
-				});
-				people.style.backgroundColor = 'lightBlue';
-
-				const isToBeRemoved = confirm(`Remove ${name}?`);
-
-				if (!isToBeRemoved) {
-					people.style.backgroundColor = 'green';
-				} else {
-					removeDropdown.click();
-					people.style.backgroundColor = 'red';
-					await remove(people);
-				}
-			}
-			return resolve();
-		});
-	}
-
-	async function remove(people) {
-		return new Promise((resolve) => {
-			setTimeout(async () => {
-				const removeButton = people.getElementsByTagName('span')[6];
-				if (removeButton.innerText === removeText) {
-					removeButton.click();
-					await confirmRemoval();
-					return resolve();
-				} else {
-					for (const span of people.getElementsByTagName('span')) {
-						if (removeButton.innerText === removeText) {
-							removeButton.click();
-							await confirmRemoval();
-							return resolve();
-						}
-					}
-				}
-			}, 200);
-		});
-	}
-
-	async function confirmRemoval() {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				const modal = document.getElementsByClassName(modalClassName)[0];
-				const confirmButton = modal.getElementsByClassName(
-					confirmRemoveButtonClassName,
-				)[1];
-				confirmButton.click();
-				return resolve();
-			}, 200);
-		});
+		const listOfConnections = getListOfConnections(document);
+		const connections = getConnectionsFromList(listOfConnections);
+		if (connections.length > 0) {
+			await iterateOverNetwork(connections);
+		} else {
+			stop = true;
+		}
 	}
 })();
+
+/**
+ * ---------------------------------
+ * MAIN FUNCTIONS
+ * ---------------------------------
+ */
+async function iterateOverNetwork(connections) {
+	return new Promise(async (resolve) => {
+		for (const connection of connections) {
+			scrollTo(connection);
+			makeConnectionBlue(connection);
+			const isToBeRemoved = askToRemove(connection);
+			if (isToBeRemoved) {
+				await remove(connection);
+			} else {
+				keep(connection);
+			}
+		}
+		return resolve();
+	});
+}
+
+async function remove(connection) {
+	return new Promise((resolve) => {
+		openRemoveDropdown(connection);
+		makeConnectionRed(connection);
+		setTimeout(async () => {
+			await confirmRemoval(connection);
+			return resolve();
+		}, awaitTime);
+	});
+}
+
+async function confirmRemoval(connection) {
+	return new Promise((resolve) => {
+		clickRemoveButton(connection);
+		setTimeout(() => {
+			clickConfirmRemoval(document)
+			return resolve();
+		}, awaitTime);
+	});
+}
+
+function keep(connection) {
+	return makeConnectionGreen(connection);
+}
+
+/**
+ * ---------------------------------
+ * HELPERS
+ * ---------------------------------
+ */
+function getListOfConnections(document) {
+	return document.getElementsByClassName(listClassName)[0];
+}
+function getConnectionsFromList(list) {
+	return [...list.getElementsByTagName('li')];
+}
+function getNameFromConnection(connection) {
+	return connection.getElementsByClassName(peopleNameClassName)[0].innerText;
+}
+function getRemoveDropdown(connection) {
+	return connection.getElementsByClassName(
+		removeDropdownClassName,
+	)[0];
+}
+function scrollTo(connection) {
+	return connection.scrollIntoView({
+		behavior: 'smooth',
+		block: 'end',
+		inline: 'nearest',
+	});
+}
+function makeConnectionBlue(connection) {
+	return connection.style.backgroundColor = 'lightBlue';
+}
+function makeConnectionGreen(connection) {
+	return connection.style.backgroundColor = 'green';
+}
+function makeConnectionRed(connection) {
+	return connection.style.backgroundColor = 'red';
+}
+function askToRemove(connection) {
+	const name = getNameFromConnection(connection);
+	return confirm(`Remove ${name}?`);
+}
+function openRemoveDropdown(connection) {
+	const removeDropdown = getRemoveDropdown(connection);
+	return removeDropdown.click();
+}
+function getRemoveButton(connection) {
+	const button = connection.getElementsByTagName('span')[6];
+	if (button.innerText === removeText) {
+		return button;
+	} else {
+		for (const span of connection.getElementsByTagName('span')) {
+			if (button.innerText === removeText) {
+				return button;
+			}
+		}
+	}
+	console.error('Could not find the remove button to click, sorry! :(')
+}
+function getRemoveConfirmModal(document) {
+	return document.getElementsByClassName(modalClassName)[0];
+}
+function clickConfirmRemoval(document) {
+	const modal = getRemoveConfirmModal(document);
+	const confirmButton = modal.getElementsByClassName(
+		confirmRemoveButtonClassName,
+	)[1];
+	confirmButton.click();
+}
+function clickRemoveButton(connection) {
+	const removeButton = getRemoveButton(connection)
+	return removeButton.click();
+}
+
+/**
+ * ---------------------------------
+ * VOCABULARY
+ * ---------------------------------
+ * - Connection: a user we are connected to, like a Facebook friend
+ */
